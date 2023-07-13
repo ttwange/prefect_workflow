0ebf656e4d2d09c7f31c442398ebaad43890da11
ttwange <ttwange2@gmail.com>
2023-07-13 21:23:59 +0300

    Testing the task,flow and sub-flow functionality on prefect

--- hello.py
+++ hello.py
@@ -6,10 +6,17 @@ def create_message():
   msg = 'Hello from prefect!!'
   return msg
 
+@flow
+def something_else():
+  result =10
+  return(result)
+
 
 @flow
 def hello_world():
+  sub_flow_message = something_else()
   task_message = create_message()
-  print(task_message)
+  new_message = task_message + str(sub_flow_message)
+  print(new_message)
 
 hello_world()
