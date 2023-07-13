7a3d25ff6f54fec2e481bcb2d43c9677b457555b
ttwange <ttwange2@gmail.com>
2023-07-13 21:19:08 +0300

    Testing the task and flow functionality on prefect

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
-from prefect import flow
+from prefect import flow,task
+
+
+@task
+def create_message():
+  msg = 'Hello from prefect!!'
+  return msg
 
 
 @flow
 def hello_world():
-  print('Hello from prefect!!')
+  task_message = create_message()
+  print(task_message)
 
 hello_world()
